c3289c4cdafb41e3c01425b696940a18ecd8e0e1
Kohsuke Kawaguchi <kk@kohsuke.org>
2020-02-23 12:28:14 -0800

    Avoid screen flicker

--- joystick.py
+++ joystick.py
@@ -9,11 +9,16 @@ pygame.joystick.init()
 j = pygame.joystick.Joystick(0)
 j.init()
 
+print(j.get_name())
 
 while True:
     pygame.event.pump()
-    os.system("clear")
-    print("%s axis=%d"%(j.get_name(),j.get_numaxes()))
+    print(" axis=%d"%(j.get_numaxes()), end="")
     for i in range(j.get_numaxes()):
-        print("%+0.2f"%(j.get_axis(i)))
+        print(" %+0.2f"%(j.get_axis(i)), end="")
+
+    print(" buttons=%d"%(j.get_numbuttons()), end="")
+    for i in range(j.get_numbuttons()):
+        print(" %d"%(j.get_button(i)), end="")
+    print(end="\r")
     time.sleep(0.1)
